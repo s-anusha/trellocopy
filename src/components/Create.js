16b6c498a87d10dc5caf9690f75b7da97f592a90
s-anusha <anusha16212@gmail.com>
2019-04-28 14:02:14 +0530

    Update README.md

--- src/components/Create.js
+++ src/components/Create.js
@@ -55,39 +55,6 @@ class Create extends React.PureComponent {
     }
   };
 
-/*
-
-  renderOpenForm = () => {
-    const { list } = this.props;
-
-    const buttonText = list ? "Add another list" : "Add another card";
-    const buttonTextOpacity = list ? 1 : 0.5;
-    const buttonTextColor = list ? "white" : "inherit";
-    const buttonTextBackground = list ? "rgba(0,0,0,.15)" : "inherit";
-
-    const OpenFormButton = styled.div`
-      display: flex;
-      align-items: center;
-      cursor: pointer;
-      border-radius: 3px;
-      height: 36px;
-      margin-left: 8px;
-      width: 300px;
-      padding-left: 10px;
-      padding-right: 10px;
-      opacity: ${buttonTextOpacity};
-      color: ${buttonTextColor};
-      background-color: ${buttonTextBackground};
-    `;
-
-    return (
-      <OpenFormButton onClick={this.openForm}>
-        <Icon>add</Icon>
-        <p style={{ flexShrink: 0 }}>{buttonText}</p>
-      </OpenFormButton>
-    );
-  };*/
-
   render() {
     const { text } = this.state;
     const { list } = this.props;
